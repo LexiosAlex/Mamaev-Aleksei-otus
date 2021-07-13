ec10667f2e6e9c28183f9b9b6c6a50fb4ea78a1b
LexiosAlex <lexiosma@gmail.com>
2021-07-13 03:45:11 +0300

    node-1: added initial files

---
+++ task-1/index.js
@@ -0,0 +1,7 @@
+import {dirReader} from "./utils/dirReader.js";
+import {parseTree} from "./utils/parseTree.js";
+const dirPath = process.argv[2];
+const depth = process.argv[3];
+
+const data = dirReader(dirPath);
+parseTree(data, depth);
