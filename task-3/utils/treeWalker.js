35c0245a417749d776ac0d88cbf089bc7dc10972
LexiosAlex <lexiosma@gmail.com>
2021-07-30 20:25:25 +0300

    node-3: treeWalker tests

--- task-3/utils/treeWalker.js
+++ task-3/utils/treeWalker.js
@@ -3,32 +3,35 @@ const NEXT = "│";
 const SPACE = "   ";
 const CROSS = "├──";
 
-const treeWalker = ({
-  treeNode,
-  prefix = "",
-  childLength = 1,
-  depth,
-  lastItemDepth
-}) => {
-  if (depth < lastItemDepth) return null;
+const moduleObj = {
+  treeWalker: ({
+    treeNode,
+    prefix = "",
+    childLength = 1,
+    depth,
+    lastItemDepth
+  }) => {
+    if (depth < lastItemDepth) return null;
 
-  treeNode.map((it, index) => {
-    const isLast = childLength - 1 === index;
+    treeNode.map((it, index) => {
+      const isLast = childLength - 1 === index;
 
-    if (isLast) console.log(prefix + LAST + it.name);
-    else console.log(prefix + CROSS + it.name);
+      if (isLast) console.log(prefix + LAST + it.name);
+      else console.log(prefix + CROSS + it.name);
 
-    it?.items &&
-      treeWalker({
-        treeNode: it.items,
-        childLength: it.items.length,
-        prefix: isLast ? prefix + SPACE : prefix + NEXT + SPACE,
-        depth: depth,
-        lastItemDepth: lastItemDepth + 1
-      });
+      if (it.items) {
+        moduleObj.treeWalker({
+          treeNode: it.items,
+          childLength: it.items.length,
+          prefix: isLast ? prefix + SPACE : prefix + NEXT + SPACE,
+          depth: depth,
+          lastItemDepth: lastItemDepth + 1
+        });
+      }
 
-    return null;
-  });
+      return null;
+    });
+  }
 };
 
-exports.treeWalker = treeWalker;
+module.exports = moduleObj;
